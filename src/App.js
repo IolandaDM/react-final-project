02664310c7326cb998e3eddbb1e3228bb93f78e2
IolandaDM <68613203+IolandaDM@users.noreply.github.com>
2020-09-26 10:10:48 +0100

    Add GitHub url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ export default function App() {
   return (
     <div className="App">
       WeatherApp
+      <footer>
+        This app was coded by Iolanda Magalhães and is {""}
+        <a href="https://github.com/IolandaDM/react-final-project" target="_blank">
+          open-sourced on Github
+        </a>
+
+
+      </footer>
     </div>
+
   );
 }
 
