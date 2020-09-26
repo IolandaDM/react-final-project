094b1406491075954aaf01739029be455208c5fb
IolandaDM <68613203+IolandaDM@users.noreply.github.com>
2020-09-26 10:22:32 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from 'react';
 export default function App() {
   return (
     <div className="App">
-      WeatherApp
+      <div className="container">
+        WeatherApp
       <footer>
-        This app was coded by Iolanda Magalhães and is {""}
-        <a href="https://github.com/IolandaDM/react-final-project" target="_blank">
-          open-sourced on Github
+          This app was coded by Iolanda Magalhães and is {""}
+          <a href="https://github.com/IolandaDM/react-final-project" target="_blank" rel="noopener noreferrer">
+            open-sourced on Github
         </a>
-
-
-      </footer>
-    </div>
+        </footer>
+      </div >
+    </div >
 
   );
 }
