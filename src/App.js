58efb81ebd6f5b180ab9cf97bb06c3ad38b7d882
IolandaDM <68613203+IolandaDM@users.noreply.github.com>
2020-09-30 20:16:27 +0100

    Add API Integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Weather from "./Weather"
+import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-
-        <Weather />
-        <footer>
+        <Weather defaultCity="New York" />
+        <footer className="github-reference">
           This app was coded by Iolanda Magalhães and is {""}
-          <a href="https://github.com/IolandaDM/react-final-project" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/IolandaDM/react-final-project" target="_blank" rel="noopener noreferrer" className="github-link">
             open-sourced on Github
         </a>
         </footer>
